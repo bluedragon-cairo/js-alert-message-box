2c216a53488dbb0180f6aeb6e389f0171e560bc1
bluedragon-cairo <76606635+bluedragon-cairo@users.noreply.github.com>
2024-05-05 17:29:19 +0900

    version

--- bootstrap.js
+++ bootstrap.js
@@ -4,61 +4,62 @@ const stringBundles = Services.strings.createBundle('chrome://jsalertmsgbox/loca
 
 const observers = {
 	contentCreated: {
-		observe(win, topic) {
-			if(!(win instanceof Ci.nsIDOMWindow && topic == 'content-document-global-created')) return;
+		observe(aWindow, aTopic) {
+			if(!(aWindow instanceof Ci.nsIDOMWindow && aTopic == 'content-document-global-created')) return;
+			try {
+				aWindow.QueryInterface(Ci.nsIDOMChromeWindow);
+				return;
+			} catch(e) {}
 			
 			var defaultTitle = '[' + stringBundles.GetStringFromName('defaultTitleUntitled') + ']'
-			if(win.wrappedJSObject.location && win.wrappedJSObject.location.hostname)
-				defaultTitle = stringBundles.GetStringFromName('defaultTitle').replace('$HOST', win.wrappedJSObject.location.protocol + '//' + win.wrappedJSObject.location.hostname);
+			if(aWindow.wrappedJSObject.location && aWindow.wrappedJSObject.location.hostname)
+				defaultTitle = stringBundles.GetStringFromName('defaultTitle').replace('$HOST', aWindow.wrappedJSObject.location.protocol + '//' + aWindow.wrappedJSObject.location.hostname);
 			const chklbl = stringBundles.GetStringFromName('checklabel');
 			var alertCount = 0, confirmCount = 0;
 			var noalert = false, noconfirm = false;
 			
-			win.wrappedJSObject._OLDALERT_alert = function alert(prpt = '', title = defaultTitle) {
+			const uid = Math.round(Math.random() * 100000).toString();
+			
+			aWindow.wrappedJSObject['_OLDALERT_alert_' + uid] = function alert(prpt = '', title = defaultTitle) {
 				if(noalert) return;
 				++alertCount;
 				if(alertCount < 2)
-					return Services.prompt.alert(win, title, prpt);
+					return Services.prompt.alert(aWindow, title, prpt);
 				const dontPrompt = { value: false };
-				const retval = Services.prompt.alertCheck(win, title, prpt, chklbl, dontPrompt);
+				const retval = Services.prompt.alertCheck(aWindow, title, prpt, chklbl, dontPrompt);
 				if(dontPrompt.value)
 					noalert = true;
 				return retval;
 			};
 			
-			win.wrappedJSObject._OLDALERT_confirm = function confirm(prpt = '', title = defaultTitle) {
+			aWindow.wrappedJSObject['_OLDALERT_confirm_' + uid] = function confirm(prpt = '', title = defaultTitle) {
 				if(noconfirm) return false;
 				++confirmCount;
 				if(confirmCount < 2)
-					return Services.prompt.confirm(win, title, prpt);
+					return Services.prompt.confirm(aWindow, title, prpt);
 				const dontPrompt = { value: false };
-				const retval = Services.prompt.confirmCheck(win, title, prpt, chklbl, dontPrompt);
+				const retval = Services.prompt.confirmCheck(aWindow, title, prpt, chklbl, dontPrompt);
 				if(dontPrompt.value)
 					noconfirm = true;
 				return retval;
 			};
 			
-			win.wrappedJSObject._OLDALERT_prompt = function prompt(prpt = '', value = '', title = defaultTitle) {
+			aWindow.wrappedJSObject['_OLDALERT_prompt_' + uid] = function prompt(prpt = '', value = '', title = defaultTitle) {
 				const input = { value };
-				const ret = Services.prompt.prompt(win, title, prpt, input, null, { value: false });
+				const ret = Services.prompt.prompt(aWindow, title, prpt, input, null, { value: false });
 				if(!ret) return null;
 				return input.value;
 			};
 			
-			try {
-				win.QueryInterface(Ci.nsIDOMChromeWindow);
-				return;
-			} catch(e) {}
-			
-			const sandbox = new Cu.Sandbox(win, {
-				sameZoneAs: win,
-				sandboxPrototype: win,
+			const sandbox = new Cu.Sandbox(aWindow, {
+				sameZoneAs: aWindow,
+				sandboxPrototype: aWindow,
 				wantXrays: false,
 			});
 			
-			Cu.evalInSandbox('window.alert = function alert(message, title) { return _OLDALERT_alert(message, title); };', sandbox);
-			Cu.evalInSandbox('window.confirm = function confirm(message, title) { return _OLDALERT_confirm(message, title); };', sandbox);
-			Cu.evalInSandbox('window.prompt = function prompt(message, def, title) { return _OLDALERT_prompt(message, def, title); };', sandbox);
+			Cu.evalInSandbox('window.alert = function alert(message, title) { return _OLDALERT_alert_' + uid + '(message, title); };', sandbox);
+			Cu.evalInSandbox('window.confirm = function confirm(message, title) { return _OLDALERT_confirm_' + uid + '(message, title); };', sandbox);
+			Cu.evalInSandbox('window.prompt = function prompt(message, def, title) { return _OLDALERT_prompt_' + uid + '(message, def, title); };', sandbox);
 			
 			Cu.evalInSandbox('window.alert.toString = function toString() { return "function alert() {\\n    [native code]\\n}"; };', sandbox);
 			Cu.evalInSandbox('window.confirm.toString = function toString() { return "function confirm() {\\n    [native code]\\n}"; };', sandbox);
